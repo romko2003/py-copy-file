7cb7e3924a48ee5244a8aaddaf234d4a405bb911
= <=>
2025-05-15 12:31:41 +0300

    implement py-copy-file

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ def copy_file(command: str):
     parts = command.split()
 
     if parts[:1] != ["cp"] or len(parts) != 3:
-        return  # Невірна команда
+        return
 
     _, source_file, target_file = parts
 
     if source_file == target_file:
-        return  # Не копіюємо файл у самого себе
+        return
 
     try:
         with open(source_file, "rb") as src_file, open(target_file, "wb") as dst_file:
             dst_file.write(src_file.read())
     except FileNotFoundError:
-        pass  # Якщо немає джерельного файлу — нічого не робимо
+        pass
